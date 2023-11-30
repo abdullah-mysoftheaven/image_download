1d2cad88605d63873557eafbd0633622e5191a8c
abdullah-mysoftheaven <abdullah.mysoftheaven@gmail.com>
2023-11-30 12:10:35 +0600

    Initial commit

---
+++ lib/utils/assets.dart
@@ -0,0 +1,106 @@
+class Assets{
+
+  static const String logo = "assets/images/logo.png";
+
+  static const String authFooter = "assets/images/footer.png";
+
+  static const String authFooter2 = "assets/images/footer2.png";
+
+  static const String farmer = "assets/images/farmer.png";
+
+  static const String farmer2 = "assets/images/farmer2.png";
+
+  static const String retails = "assets/images/retails.png";
+
+  static const String nidHand = "assets/images/nid_hand.png";
+
+  static const String govLogo = "assets/images/gov_logo.png";
+
+  static const String romana = "assets/images/romana.png";
+
+  static const String cross = "assets/images/cross.png";
+
+  static const String p = "assets/images/p.png";
+
+  static const String onion = "assets/images/onion.png";
+
+  static const String chili = "assets/images/chili.png";
+
+  static const String tomato = "assets/images/tomato.png";
+
+  static const String profileAvater = 'assets/images/profile_avater.jpg';
+  static const String messageBgImage = 'assets/images/message_bg_image.png';
+
+  static const String seed = "assets/images/tomato.png";
+
+  static const String seed2 = "assets/images/seed2.png";
+  static const String image_category = "assets/images/image_category.png";
+
+  /// icon
+
+  static const String dashboardLogo = 'assets/icon/dash_logo.png';
+
+  static const String nilamLogo = 'assets/icon/nilam_logo.png';
+
+  static const String ponnoLogo = 'assets/icon/ponno_logo.png';
+
+  static const String otherLogo = 'assets/icon/other.png';
+
+  static const String menuLogo = 'assets/icon/menu.png';
+
+  static const String back = 'assets/icon/back.png';
+
+  static const String krishiPonno = 'assets/icon/krishi_ponno.png';
+
+  static const String qnaLogo = 'assets/icon/qna.png';
+
+  static const String instructionLogo = 'assets/icon/instruction.png';
+
+  static const String cardLogo = 'assets/icon/card_icon.png';
+
+  static const String category = 'assets/icon/category.png';
+
+  static const String filter = 'assets/icon/filter.png';
+
+  static const String edit = 'assets/icon/edit.png';
+  static const String govLogo1 = 'assets/icon/gov_logo.png';
+  static const String notificationIcon = 'assets/icon/notification_icon.png';
+  static const String communicationSomporkitoIcon = 'assets/icon/faq/communication_somporkito_icon.png';
+  static const String delivarySomporkitoIcon = 'assets/icon/faq/delivary_somporkito_icon.png';
+  static const String lendenSomporkitoIcon = 'assets/icon/faq/lenden_somporkito_icon.png';
+  static const String otherSomporkitoIcon = 'assets/icon/faq/other_question_icon.png';
+  static const String ponnoSomporkitoIcon = 'assets/icon/faq/ponno_somporkito_icon.png';
+  static const String profileSomporkitoIcon = 'assets/icon/faq/profile_somporkito_icon.png';
+
+  static const String tkLogo = 'assets/icon/tk_icon.png';
+  static const String editIcon = 'assets/icon/edit_icon.png';
+  static const String forgotPassIcon = 'assets/icon/icon_forgot_pass.png';
+
+  static const String icon_nav_ask_question = 'assets/icon/icon_nav_ask_question.png';
+  static const String icon_nav_menual = 'assets/icon/icon_nav_menual.png';
+  static const String icon_nav_ponno_category = 'assets/icon/icon_nav_ponno_catagory.png';
+  static const String icon_nav_profile = 'assets/icon/icon_nav_profile.png';
+  static const String icon_nav_search = 'assets/icon/icon_nav_search.png';
+
+  static const String icon_nav_dashboard = 'assets/icon/icon_nav_dashboard.png';
+  static const String icon_nav_krishi_ponno = 'assets/icon/icon_nav_krishi_ponno.png';
+  static const String icon_nav_nilam_dor = 'assets/icon/icon_nav_nilam_dor.png';
+  static const String icon_nav_ponno_poribahan = 'assets/icon/icon_nav_ponno_poribahan.png';
+  static const String icon_nav_ponno_sonrokkito = 'assets/icon/icon_nav_ponno_sonrokkito.png';
+
+
+  static const String icon_nilam = 'assets/icon/icon_nilam.png';
+  static const String icon_paikary = 'assets/icon/icon_paikary.png';
+  static const String icon_daily_bazar = 'assets/icon/icon_daily_bazar.png';
+
+
+  static const String icon_taka = 'assets/icon/icon_taka.png';
+  static const String icon_location = 'assets/icon/icon_location.png';
+
+  static const String icon_all = 'assets/icon/icon_all.png';
+
+
+  /// animation
+
+  static const String success = 'assets/animation/success.json';
+}
